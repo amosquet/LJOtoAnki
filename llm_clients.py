d25cff56eb9b140e370e5990d3ba75fcd09b7928
AlmondMan <artusmosquet@gmail.com>
2026-03-13 13:36:26 -0400

    Removed API key
    Cough Cough, I pushed my API key, oops...

--- llm_clients.py
+++ llm_clients.py
@@ -8,11 +8,10 @@ import config
 
 def call_gemini(text_input: str):
     """Sends the text to Google's Gemini model."""
-    # if not config.GEMINI_API_KEY:
-    #     return "Gemini API key missing."
+    if not config.GEMINI_API_KEY:
+        return "Gemini API key missing."
 
-    # client = genai.Client(api_key=config.GEMINI_API_KEY)
-    client = genai.Client(api_key="AIzaSyB6A8jTvGh7XGF8tFVTzfF12GxL1YIgBcg")
+    client = genai.Client(api_key=config.GEMINI_API_KEY)
     model = "gemini-2.5-pro"
     system = types.GenerateContentConfig(system_instruction=config.SYSTEM_PROMPT)
 
